a75c3e4d1a2652aeae14cf37070df34ade882d82
sotrnguy92 <sonnguyen092@gmail.com>
2020-08-08 22:35:51 -0700

    cleaned up create question function and added a way to click on dynamically created elements

--- Assets/app.js
+++ Assets/app.js
@@ -6,25 +6,34 @@ $(document).ready(function(){
     const $p = $('p');
     const $pLead = $('.lead');
 
-    let quesObj = {
-        q1: ["Inside which HTML element do we put the JavaScript?", '<javascript>', '<js>', '<script>', '<scripting>']
+    let quesObj =[ 
+    {
+        q: ["Inside which HTML element do we put the JavaScript?", '<javascript>', '<js>', '<script>', '<scripting>'],
+        answer: 2
+
     }
+]
 
     $start.on('click', function(){
         $jheading.empty();
         $p.empty();
-        $jheading.text(quesObj.q1[0]);
+        createQuestion();
+    });
+    
+    function createQuestion(questionindex){
+        $jheading.text(quesObj[0].q[0]);
         $ulist = $('<ul>').addClass('list-group list-group-flush');
         $p.append($ulist);
-        for (let i = 1; i<quesObj.q1.length; i++){
+        for (let i = 1; i<quesObj[0].q.length; i++){
             let $listel = $('<li>').addClass('text-left list-group-item');
-            $listel.html($('<button>').addClass(' btn btn-primary my-2').text(i +': ' + quesObj.q1[i]));
+            $listel.html($('<button>').addClass('answer btn btn-primary my-2').text(i +': ' + quesObj[0].q[i]).attr('index',i));
             $ulist.append($listel);
-        //     $btnlist = $('<button>');
-        //     $btnlist.
         }
+    }
 
-    });
-
+    $('.container').on('click', 'button.answer' , function(){
+        event.stopPropagation();
+        console.log($(this).attr('index'));
+    })
 
 })
